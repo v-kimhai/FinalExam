9cd8f6e4cb64ed01f178fcf185ba41e28b99d62b
vanna.kimhai <v-kimhai@users.noreply.github.com>
2020-07-22 15:07:24 +0700

    init

---
+++ app/src/main/java/com/example/vannakimhai/StartupActivity.kt
@@ -0,0 +1,12 @@
+package com.example.vannakimhai
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_startup)
+    }
+}
